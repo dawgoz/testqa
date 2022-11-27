5a37eaede48a95a49a62ee83b9c4e659dc068005
Oskaras <74023964+dawgoz@users.noreply.github.com>
2022-11-27 15:23:46 +0200

    Added Naming.java class

---
+++ Naming.java
@@ -0,0 +1,31 @@
+package az.alizeynalli.cleancode.naming;
+
+public class Naming {
+
+    public Date d;
+    public List<Testcase> find(User user){};
+    
+    public Account[] AccountList;
+
+    public void deleteUser(User pUser){};
+    
+    public URL r;
+  
+    public class TableRepresent{};
+    pubcli void car(){};
+  
+    public saveUser(){}; createAccount(){}; generateContract(){};
+
+    public class AccountFactory{}
+    public class AccountObserver{}
+    
+    public class User;
+    public String userName;  
+    public void editUser(User user){};
+
+    public void saveIfUserUnder18(){};
+    public class UserFormRealTimeFiller {};
+    
+    public int daysInYear = 360;
+    public int hoursInWeek = 168;
+}
